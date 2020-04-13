d686d52081f4fc56a3f3a07580779d6e5938d00e
Brandon Moody <brandon@thef33d.com>
2020-04-13 19:41:22 -0400

    solution 2

--- tester.js
+++ tester.js
@@ -25,3 +25,45 @@ const romanToInt = str => {
 
     return result
 }
+
+
+// SOLUTION 2
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+    const romIntMap = {
+        "I": 1,
+        "IV": 4,
+        "V": 5,
+        "IX": 9,
+        "X": 10,
+        "XL": 40,
+        "L": 50,
+        "XC": 90,
+        "C": 100,
+        "CD": 400,
+        "D": 500,
+        "CM": 900,
+        "M": 1000
+    };
+
+    let num = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i + 1] !== undefined && (s[i] === "I" || s[i] === "X" || s[i] === "C")) {
+            if (romIntMap[s[i] + s[i + 1]]) {
+                num = num + romIntMap[s[i] + s[i + 1]];
+                i++;
+            } else {
+                num = num + romIntMap[s[i]];
+            }
+        } else {
+            num = num + romIntMap[s[i]];
+        }
+    }
+
+    return num;
+};
